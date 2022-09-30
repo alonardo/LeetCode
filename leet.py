10624dfddd459da032498301d39854a9471427c3
alonardo <105623292+alonardo@users.noreply.github.com>
2022-09-30 12:50:50 -0600

    Arrays & Hashing - Easy

---
+++ leet.py
@@ -0,0 +1,44 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashset = set()
+        
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+            
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+            
+        return True
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+class Solution(object):
+    def twoSum(self, nums, target):
+        prevMap = {}
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return 
+        
+        
