a9fb8c61c25f286bde08502e239771ed7ecfd4be
alonardo <105623292+alonardo@users.noreply.github.com>
2022-10-01 09:14:00 -0600

    more hashing

--- leet.py
+++ leet.py
@@ -41,4 +41,20 @@ class Solution(object):
             prevMap[n] = i
         return 
         
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.        
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            
+            res[tuple(count)].append(s)
+            
+        return res.values()
+                
         
