eaa91c99e4c82b2a7f51dc8a638897a78e9fc0fd
alonardo <105623292+alonardo@users.noreply.github.com>
2022-10-03 18:47:33 -0600

    two pointer

--- leet.py
+++ leet.py
@@ -57,4 +57,15 @@ class Solution(object):
             
         return res.values()
                 
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+# it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.        
+
+class Solution(object):
+    def isPalindrome(self, s):
+        new_str = ""
         
+        for c in s:
+            if c.isalnum():
+                new_str += c.lower()
+        return new_str == new_str[::-1]
